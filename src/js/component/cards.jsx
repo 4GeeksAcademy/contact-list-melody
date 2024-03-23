96db53c314620c94c9d5440b91d9e1abf5c0e722
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-03-23 19:12:29 +0000

    itentando exportar cards

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React { Component } from "react";
 import { Link } from "react-router-dom";
+import profile-picture from "../../img/profile-picture.webp";
 
 export const Cards = () => (
 	<div className="card mb-3" style="max-width: 540px;">
   <div className="row g-0">
     <div className="col-md-4">
-      <img src="..." className="img-fluid rounded-start" alt="...">
+    <img className="img-fluid rounded-start" src={profile-picture} /> 
     </div>
     <div className="col-md-8">
       <div className="card-body">
